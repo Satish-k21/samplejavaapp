95bb2962f9548c02adcb6fd29ee294e544007f01
Satish-k21 <143918951+Satish-k21@users.noreply.github.com>
2024-01-14 10:56:51 +0530

    Update PrepareTargetMethod.java

--- src/main/java/com/devopsdemo/utilities/PrepareTargetMethod.java
+++ src/main/java/com/devopsdemo/utilities/PrepareTargetMethod.java
@@ -4,6 +4,7 @@ public class PrepareTargetMethod {
 
 	private static final String METHOD_GET_PREFIX = "get";
 	/***
+         * Webhooks
 	 * preparing target name of getter method for given sort field
 	 *
 	 * @param name a {@link java.lang.String}
